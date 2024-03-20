ab826a662b8d6c8853ee376241a96307cada5d07
Kaustubh Atey <45921435+Kaustubh-Atey@users.noreply.github.com>
2024-03-20 14:58:58 +0530

    Updated variable names

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ else:
                   'Support Vector Machine': 'Models/white_SVM_model.joblib',
                   'Neural Network': 'Models/white_NN_model.joblib'}[model_options]
 
+# REMOVE THIS LINE 
 st.write('Selected = ', wine_data_path, model_path)
 
 st.subheader('Slide the bars to slect value of features:', divider='rainbow')
@@ -42,21 +43,21 @@ col_11, col_12, col_13 = st.columns(3)
 with col_11:
 
     alcohol_f = st.slider('Alcohol', min_value=0.0, max_value=20.0, value=10.0)
-    total_sulfur_dioxide_f = st.slider('total sulfur dioxide', min_value=0.0, max_value=300.0, value=100.0)
-    volatile_acidity_f = st.slider('volatile acidity', min_value=0.0, max_value=3.0, value=0.5)
-    free_sulfur_dioxide_f = st.slider('free sulfur dioxide', min_value=0.0, max_value=100.0, value=50.0)
+    total_sulfur_dioxide_f = st.slider('Total Sulfur Dioxide', min_value=0.0, max_value=300.0, value=100.0)
+    volatile_acidity_f = st.slider('Volatile Acidity', min_value=0.0, max_value=3.0, value=0.5)
+    free_sulfur_dioxide_f = st.slider('Free Sulfur Dioxide', min_value=0.0, max_value=100.0, value=50.0)
     
 with col_12:
     
     pH_f = st.slider('pH', min_value=0.0, max_value=7.0, value=3.0)
-    sulphates_f = st.slider('sulphates', min_value=0.0, max_value=3.0, value=1.0)
-    citric_acid_f = st.slider('citric acid', min_value=0.0, max_value=1.0, value=0.2)
-    density_f = st.slider('density', min_value=0.8, max_value=1.2, value=0.9)
+    sulphates_f = st.slider('Sulphates', min_value=0.0, max_value=3.0, value=1.0)
+    citric_acid_f = st.slider('Citric Acid', min_value=0.0, max_value=1.0, value=0.2)
+    density_f = st.slider('Density', min_value=0.8, max_value=1.2, value=0.9)
 
 with col_13:
-    fixed_acidity_f = st.slider('fixed acidity', min_value=0.0, max_value=20.0, value=10.0)
-    residual_sugar_f = st.slider('residual sugar', min_value=0.0, max_value=20.0, value=10.0)
-    chlorides_f = st.slider('free chlorides', min_value=0.0, max_value=1.0, value=0.2)
+    fixed_acidity_f = st.slider('Fixed Acidity', min_value=0.0, max_value=20.0, value=10.0)
+    residual_sugar_f = st.slider('Residual Sugar', min_value=0.0, max_value=20.0, value=10.0)
+    chlorides_f = st.slider('Free Chlorides', min_value=0.0, max_value=1.0, value=0.2)
 
 # User input to test
 test_input = np.array([alcohol_f, total_sulfur_dioxide_f, volatile_acidity_f, fixed_acidity_f, residual_sugar_f,
@@ -67,8 +68,8 @@ wine_df = pd.read_csv(wine_data_path)
 scalar = MinMaxScaler()
 scalar.fit(wine_df)
 
-# Print the Input
-st.write('User Input: ', test_input)
+# REMOVE SOON
+#st.write('User Input: ', test_input)
 test_input = scalar.transform(test_input)
 
 # Load model
